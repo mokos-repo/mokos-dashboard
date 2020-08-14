aff888fc975f28e7bb5790994b52c2c848afd449
manny <aman.teferi.80@gmail.com>
2020-08-14 10:45:34 +0300

    (fix) create menu items with ingredients feature added

--- src/screens/CreateMenuItem/queries.js
+++ src/screens/CreateMenuItem/queries.js
@@ -3,14 +3,16 @@ import gql from 'graphql-tag'
 export const CREATE_MENU_ITEM = gql`
     mutation create_menu_item(
         $hotspot: ID, $title: String, $description: String, 
-        $image: String, $price: String, $tags: [MenuItemTagCreateInput]){
+        $image: String, $price: String, $tags: [MenuItemTagCreateInput],
+        $ingredients: [CreateIngredientInput]){
         createMenuItem(data:{
             hotspot: $hotspot,
             title: $title,
             description: $description,
             image: $image,
             price: $price,
-            tags: $tags
+            tags: $tags,
+            ingredients: $ingredients
         }){
             id
         }
